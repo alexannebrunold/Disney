d92fba90d948d40263c902a25bf37e85fd11d84e
j2frise <jarce2frise@yahoo.fr>
2019-11-29 07:00:16 +0100

    Update

--- assets/js/newspage3.js
+++ assets/js/newspage3.js
@@ -1,39 +1,48 @@
 var clickone = document.getElementById("clickone");
-var clictwo = document.getElementById("clicktwo");
-var clicthree = document.getElementById("clickthree");
+var clicktwo = document.getElementById("clicktwo");
+var clickthree = document.getElementById("clickthree");
 var first = document.getElementById("first");
 var second = document.getElementById("second");
 var third = document.getElementById("third");
-clickone.addEventListener("click", function() {
-  var nb = clickone.getAttribute("rel");
-  if (nb == 0) {
-    clickone.innerHTML = '<i class="icofont-rounded-up"></i>';
-    first.classList.remove("hide-block");
-    first.classList.add("show-block");
-    clickone.setAttribute("rel", "1");
-  } else {
-    clickone.innerHTML = '<i class="icofont-rounded-down"></i>';
-    first.classList.remove("show-block");
-    first.classList.add("hide-block");
-    clickone.setAttribute("rel", "0");
-  }
-});
 
-clicktwo.addEventListener("click", function() {
-  var nb = clicktwo.getAttribute("rel");
-  if (nb == 0) {
-    clicktwo.innerHTML = '<i class="icofont-rounded-up"></i>';
-    second.classList.remove("hide-block");
-    second.classList.add("show-block");
-    clicktwo.setAttribute("rel", "1");
-  } else {
-    clicktwo.innerHTML = '<i class="icofont-rounded-down"></i>';
-    second.classList.remove("show-block");
-    second.classList.add("hide-block");
-    clicktwo.setAttribute("rel", "0");
-  }
-});
+if(clickone){
+  clickone.addEventListener("click", function() {
+    var nb = clickone.getAttribute("rel");
+    if (nb == 0) {
+      clickone.innerHTML = '<i class="icofont-rounded-up"></i>';
+      first.classList.remove("hide-block");
+      first.classList.add("show-block");
+      clickone.setAttribute("rel", "1");
+    } else {
+      clickone.innerHTML = '<i class="icofont-rounded-down"></i>';
+      first.classList.remove("show-block");
+      first.classList.add("hide-block");
+      clickone.setAttribute("rel", "0");
+    }
+  });
+  
+}
 
+
+if(clicktwo){
+  clicktwo.addEventListener("click", function() {
+    var nb = clicktwo.getAttribute("rel");
+    if (nb == 0) {
+      clicktwo.innerHTML = '<i class="icofont-rounded-up"></i>';
+      second.classList.remove("hide-block");
+      second.classList.add("show-block");
+      clicktwo.setAttribute("rel", "1");
+    } else {
+      clicktwo.innerHTML = '<i class="icofont-rounded-down"></i>';
+      second.classList.remove("show-block");
+      second.classList.add("hide-block");
+      clicktwo.setAttribute("rel", "0");
+    }
+  });
+}
+
+if(clickthree){
+  
 clickthree.addEventListener("click", function() {
   var nb = clickthree.getAttribute("rel");
   if (nb == 0) {
@@ -48,3 +57,5 @@ clickthree.addEventListener("click", function() {
     clickthree.setAttribute("rel", "0");
   }
 });
+
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,86 @@ function close_menu() {
   mobileMenu.classList.add("is-close");
   mobileMenu.classList.remove("is-open");
 }
+
+var visual = document.getElementById('visual');
+var img_product = document.querySelectorAll('.img-product');
+for(let i = 0; i<img_product.length; i++){
+  img_product[i].addEventListener('click',function (){
+    var change = img_product[i].getAttribute('src');
+    
+    visual.setAttribute('src',change);
+  } )
+}
+
+if(visual){
+  visual.addEventListener("click",function(){
+    location.href="pages/shop.html";
+  });
+}
+
+var btn = document.getElementById('contact');
+if(btn){
+
+  btn.addEventListener('click',function (){
+    var require = document.querySelectorAll('.require');
+    var retour = document.getElementById('retour');
+    var error = 0;
+    for(let j = 0; j<require.length; j++){
+      var val = require[j].value;
+  
+      if(val.trim() == ""){
+        require[j].classList.add('error');
+        error++;
+      }
+      else{
+        require[j].classList.remove('error');
+      }
+    }
+  
+    if(error == 0){  
+      retour.innerHTML='<div class="alert success"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>Message envoyé<br>Merci de nous avoir contacté</div>';
+      for(let k = 0; k<require.length; k++){
+        require[k].value="";
+      }
+    
+    }
+    else{
+      retour.innerHTML='<div class="alert danger"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>Tous les champs sont obligatoires</div>';
+    }
+  
+  } );
+}
+
+
+var connexion = document.getElementById('login');
+if(connexion){
+  
+connexion.addEventListener('click',function (){
+  var require = document.querySelectorAll('.require');
+  var retour = document.getElementById('retour');
+  var error = 0;
+  for(let l = 0; l<require.length; l++){
+    var val = require[l].value;
+
+    if(val.trim() == ""){
+      require[l].classList.add('error');
+      error++;
+    }
+    else{
+      require[l].classList.remove('error');
+    }
+  }
+
+  if(error == 0){  
+    retour.innerHTML='<div class="alert success"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>Connexion réussit</div>';
+    for(let m = 0; m<require.length; m++){
+      require[m].value="";
+    }
+  }
+  else{
+    retour.innerHTML='<div class="alert danger"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>Tous les champs sont obligatoires</div>';
+  }
+
+});
+
+}
